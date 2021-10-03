656bb8e3d42941c458c384ac66d21e5338faaebd
parvej1999 <55800105+parvej1999@users.noreply.github.com>
2021-10-03 12:49:27 +0530

    Update knightTour.cpp

--- knightTour.cpp
+++ knightTour.cpp
@@ -18,6 +18,42 @@ bool valid(int sx, int sy, int board[n][n]){
     return false;
 }
 
+bool solve(int board[n][n], int sx, int sy, int mv, int dx[n], int dy[n]){
+    if(mv == n*n)return true;
+
+    
+    for(int i=0; i<8; i++){
+        int x = sx + dx[i], y = sy + dy[i];
+        // clrscr();
+        // printBoard(board);
+        // system("clear");
+        if(valid(x,y, board) && board[x][y] == -1){
+            board[x][y] = mv;
+            if(solve(board, x, y, mv+1, dx, dy)){
+                return true;
+            }
+            else
+                board[x][y] = -1;
+        }
+    }
+   
+    return false;
+}
+
+int main(){
+    int board[n][n];
+    memset(board, -1, sizeof(board));
+    board[0][0] = 0;
+    int dx[8] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+    int dy[8] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+    
+    if(solve(board, 0, 0, 1, dx, dy)){
+        printBoard(board);
+    }
+    return 0;
+}
+
+
 
 
 
